40298ee32ff2b640581dc51fe3da64a3a7810d67
Kirill Müller <krlmlr+r@mailbox.org>
2020-01-11 16:21:48 +0100

    Add test

--- tests/testthat/test-comparison.R
+++ tests/testthat/test-comparison.R
@@ -12,3 +12,10 @@ test_that("basic io works", {
   expect_equal(rep(nrow(spectrum), 2), dim(compare_colour(spectrum, from_space = 'rgb')))
   expect_equal(c(nrow(spectrum), nrow(spectrum2)), dim(compare_colour(spectrum, spectrum2, from_space = 'rgb')))
 })
+
+test_that("blue is included in Euclidean comparison (#20)", {
+  expect_equal(
+    compare_colour(decode_colour("#000000"), decode_colour("#0000FF"), "rgb")[1, 1],
+    255
+  )
+})
