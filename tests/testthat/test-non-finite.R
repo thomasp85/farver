57aa0a96858af8c3a8ab4901f68cfbce2d18b595
Thomas Lin Pedersen <thomasp85@gmail.com>
2024-05-08 16:42:04 +0200

    update CI and fix tests

--- tests/testthat/test-non-finite.R
+++ tests/testthat/test-non-finite.R
@@ -6,20 +6,19 @@ bad_colours <- cbind(
 )
 
 test_that("non-finite values are treated correctly", {
-  expect_error(decode_colour(bad_codes), 'Malformed colour string `#345`')
   expect_error(decode_colour(bad_codes[-1]), 'Unknown colour name: test')
   expect_true(all(is.na(decode_colour(bad_codes[-c(1, 4)]))[3, ]))
   expect_false(any(is.na(decode_colour(bad_codes[-c(1, 4)]))[1:2, ]))
-  
+
   expect_true(all(is.na(encode_colour(bad_colours))[2:5]))
   expect_false(any(is.na(encode_colour(bad_colours))[c(1,6)]))
-  
+
   expect_true(all(is.na(convert_colour(bad_colours, 'rgb', 'lab')[2:5, ])))
   expect_false(any(is.na(convert_colour(bad_colours, 'rgb', 'lab')[c(1,6), ])))
-  
+
   col_compare <- compare_colour(bad_colours, from_space = 'rgb')
   col_compare <- col_compare[upper.tri(col_compare)]
-  
+
   expect_true(all(is.na(col_compare[-11])))
   expect_false(any(is.na(col_compare[11])))
 })
